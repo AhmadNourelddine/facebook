2d3e0c44c7420dff2a903d68aa316298e2107cf2
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-02-15 01:24:55 +0200

    updates on api post

--- api/login.php
+++ api/login.php
@@ -1,26 +1,24 @@
 <?php
 include("db_info.php");
 
-if(isset($_GET['email']) && isset($_GET['pass']))
-{
-$email = $_GET['email'];
-$pass = $_GET['pass'];
-}
-else{
-  die("no required information");
-}
+$array = json_decode(file_get_contents("php://input"), true);
+// print_r($array);
+$email = $array['email'];
+$pass = $array['pass'];
+
 $check_user_exist = $mysqli-> prepare("select * from users where email=? and password=?");
 $check_user_exist->bind_param("ss",$email,$pass);
 $check_user_exist-> execute();
 $array =  $check_user_exist-> get_result();
 $user_info=[];
-if($array->num_rows>0)
+if($array->num_rows > 0)
 {
   $user_info['status']=true;
   $user = $array->fetch_assoc();
+    // $user_info['user_info']=$user;
   foreach ($user as $key => $value) {
      $user_info[$key] = $value;
-}
+  }
 }
 else{
   $user_info['status']=false;
