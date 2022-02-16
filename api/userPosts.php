694073da01bc47013afff389117039097224a219
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-02-16 17:04:57 +0200

    edits in api

--- api/userPosts.php
+++ api/userPosts.php
@@ -12,7 +12,7 @@ else {
 }
 
 
-$get_friends_id = $mysqli->prepare("SELECT p.*,count(l.user_like_id) likes from posts p left join likes l on l.post_id=p.id where p.user_id=? group by p.id");
+$get_friends_id = $mysqli->prepare("SELECT p.*,count(l.user_like_id) likes from posts p left join likes l on l.post_id=p.id where p.user_id=? group by p.id order by p.post_date desc");
 $get_friends_id->bind_param("s",$id);
 $get_friends_id-> execute();
 $array =  $get_friends_id-> get_result();
